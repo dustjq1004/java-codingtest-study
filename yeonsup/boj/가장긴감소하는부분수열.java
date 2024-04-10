d552a2703d7768e7aef949fef12b2d7ef1c627a3
dustjq1004 <dustjq1005@naver.com>
2024-04-10 17:23:23 +0900

    [BOJ] 가장 긴 감소하는 부분 수열 / 실버 2 / 50m https://www.acmicpc.net/problem/11722

---
+++ yeonsup/boj/가장긴감소하는부분수열.java
@@ -0,0 +1,32 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 가장긴감소하는부분수열 {
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int[] arr = new int[n];
+        dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = n - 1; j >= i; j--) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
